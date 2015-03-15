371de7d362e5e0684d974067acbaf3a6ed4b5353
jedireza <reza@akhavan.me>
2015-03-14 18:58:52 -0700

    tabs => spaces + syntax changes

--- test/transform.js
+++ test/transform.js
@@ -1,13 +1,21 @@
 var Babel = require('babel-core');
 
-module.exports = [
-	{ ext: '.jsx', transform: function (content, filename) {
 
-		if (filename.indexOf('node_modules') === -1) {
-			var result = Babel.transform(content, { sourceMap: 'inline', filename: filename, sourceFileName: filename });
-			return result.code;
-		}
+module.exports = [{
+    ext: '.jsx',
+    transform: function (content, filename) {
 
-		return content;
-	}}
-];
+        if (filename.indexOf('node_modules') === -1) {
+            var options = {
+                sourceMap: 'inline',
+                filename: filename,
+                sourceFileName: filename
+            };
+            var result = Babel.transform(content, options);
+
+            return result.code;
+        }
+
+        return content;
+    }
+}];
