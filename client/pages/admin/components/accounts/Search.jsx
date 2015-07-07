d44c1accae80793be148ea49e9879978f11f3f29
jedireza <reza@akhavan.me>
2015-07-06 22:15:32 -0700

    fix react-router transtionTo calls

--- client/pages/admin/components/accounts/Search.jsx
+++ client/pages/admin/components/accounts/Search.jsx
@@ -50,7 +50,7 @@ var Component = React.createClass({
             event.stopPropagation();
         }
 
-        this.transitionTo('accounts', {}, this.refs.filters.state);
+        this.context.router.transitionTo('accounts', {}, this.refs.filters.state);
         window.scrollTo(0, 0);
     },
     onPageChange: function (page) {
