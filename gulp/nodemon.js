d4a4e8c1f05641b78bdd9917c06836ef8175effc
Han Lhan <Han Lhan>
2017-01-13 16:32:47 +0800

    add vs code debug attach, env config optimize

--- gulp/nodemon.js
+++ gulp/nodemon.js
@@ -1,14 +1,20 @@
 'use strict';
 const Gulp = require('gulp');
 const Nodemon = require('gulp-nodemon');
+const Config = require('./../config.js');
 
 
 Gulp.task('nodemon', () => {
 
-    const nodeArgs = ['--inspect'];
+    const nodeArgs =[];
+    
+    if(process.env.INSPECTER){
+        const nodeArgs = ['--inspect'];
+    }
 
     if (process.env.DEBUGGER) {
         nodeArgs.push('--debug');
+        console.log('debug');
     }
 
     Nodemon({
